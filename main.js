7793bb41b95713e2e820e7a8704312861a19525a
Miguel Angel Franco <mfranco_98@yahoo.com>
2018-06-14 20:56:39 -0500

    checando buscador de gifs

--- main.js
+++ main.js
@@ -45,12 +45,15 @@ bot.hear(/gif (.*)/i, (payload, chat, data)=>{
   const query = data.match[1];
   chat.say(`Buscando el mejor Gif de: ${query}`, {typing:true});
   fetch(GIPHY_URL + query)
-    .then(res => res.json())
-    .then(json =>{
-      chat.say({
-        attachment: 'image',
-        url: json.data[0].images.fixed_height.url
-      });
+  
+  .then(res => res.json())
+  .then(json =>{
+    /* chat.say({
+      attachment: 'image',
+      url: json.data[0].images.fixed_height.url */
+      chat.say(json)
+    }); 
+    
     })
 })
 
